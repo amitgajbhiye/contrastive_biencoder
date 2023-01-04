ad46d7ed15386b01673470494cb4ef2740115877
Amit Gajbhiye <amitgajbhiye>
2023-01-04 14:46:56 +0000

    data sampling 10neg75thres - cnetp pretrain

---
+++ run_get_prop_conj_data_cnetp_10neg75thres.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=10neg75
+
+#SBATCH --output=logs/data_sampling/out_pretrain_cnetp_get_predict_prop_sim_props_10neg75thresh.txt
+#SBATCH --error=logs/data_sampling/err_pretrain_cnetp_get_predict_prop_sim_props_10neg75thresh.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+##SBATCH --partition gpu_v100,gpu
+##SBATCH --mem=8g
+##SBATCH --gres=gpu:1
+
+#SBATCH --partition compute
+#SBATCH --time 0-14:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/5_get_predict_prop_sim_data/get_predict_prop_sim_props_10neg75thresh.json
+
+
+echo 'Job Finished!'
