97534ba34178e1f996a9df2fa852d01b851068a8
Amit Gajbhiye <amitgajbhiye>
2023-01-04 10:53:48 +0000

    data sampling 5neg80thres - cnetp pretrain

--- run_get_prop_conj_data_cnetp_5neg80thres.sh
+++ run_get_prop_conj_data_cnetp_5neg80thres.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=McRps5Neg50th
 
 #SBATCH --output=logs/data_sampling/out_pretrain_cnetp_get_predict_prop_sim_props_5neg80thresh.txt
-#SBATCH --error=logs/data_sampling/err_mcrae_ps_get_predict_prop_sim_props_5neg50thresh.txt
+#SBATCH --error=logs/data_sampling/err_pretrain_cnetp_get_predict_prop_sim_props_5neg80thresh.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
@@ -14,7 +14,7 @@
 ##SBATCH --gres=gpu:1
 
 #SBATCH --partition compute
-#SBATCH --time 0-6:00:00
+#SBATCH --time 0-10:00:00
 
 echo 'This script is running on:'
 hostname
@@ -25,7 +25,7 @@ module load anaconda/2020.02
 
 conda activate venv
 
-python3 get_embeds_and_train_data.py --config_file configs/5_get_predict_prop_sim_data/mcrae_ps_get_predict_prop_sim_props_5neg50thresh.json
+python3 get_embeds_and_train_data.py --config_file configs/5_get_predict_prop_sim_data/get_predict_prop_sim_props_5neg80thresh.json
 
 
 echo 'Job Finished!'
