fb2f5479055a21e12ce7447b8fa193a3da279f4e
Amit Gajbhiye <amitgajbhiye>
2023-01-09 11:29:19 +0000

    mcrae con sim 15 props

---
+++ run_mcrae_get_con_15sim_vocab_prop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=MCconSim15Emb
+
+#SBATCH --output=logs/data_sampling/out_mcrae_get_con_sim_15_vocab_props.txt
+#SBATCH --error=logs/data_sampling/err_mcrae_get_con_sim_15_vocab_props.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+##SBATCH -p gpu_v100,gpu
+##SBATCH --mem=8g
+##SBATCH --gres=gpu:1
+
+#SBATCH -p compute
+#SBATCH -t 0-04:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/mcrae_get_con_sim_15_vocab_props.json
+
+
+echo 'finished!'
