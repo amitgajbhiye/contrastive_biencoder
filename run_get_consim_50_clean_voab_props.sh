e0113b721be999dbaed9204cbcd12809bad3d3ed
Amit Gajbhiye <amitgajbhiye>
2023-01-11 10:25:47 +0000

    get_cnet_con_50sim_clean_vocab_prop

---
+++ run_get_consim_50_clean_voab_props.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=conSim50cleanPropVocab
+
+#SBATCH --output=logs/data_sampling/out_get_cnet_con_50sim_clean_vocab_prop.txt
+#SBATCH --error=logs/data_sampling/err_get_cnet_con_50sim_clean_vocab_prop.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100,gpu
+#SBATCH --mem=8g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-03:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/4_get_cnet_con_50sim_clean_vocab_prop.json
+
+
+echo 'Job Finished!'
