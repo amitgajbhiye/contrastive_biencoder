dc584ed72af1a23e45ae4e72fe91d53cd1908cef
Amit Gajbhiye <amitgajbhiye>
2023-01-12 17:16:07 +0000

    for concept split mcrae_cs_get_predict_prop_sim_props_5neg50thres

---
+++ run_mcrae_get_prop_conj_data_5neg50thres_concept_split.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=mcCS5neg50thres
+
+#SBATCH --output=logs/data_sampling/out_mcrae_cs_get_predict_prop_sim_vocab_props_pt5neg50thres.txt
+#SBATCH --error=logs/data_sampling/err_mcrae_cs_get_predict_prop_sim_vocab_props_pt5neg50thres.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+##SBATCH -p gpu_v100,gpu
+##SBATCH --mem=8g
+##SBATCH --gres=gpu:1
+
+#SBATCH -p compute
+#SBATCH -t 0-5:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/5_get_predict_prop_sim_data/mcrae_cs_get_predict_prop_sim_props_5neg50thres.json
+
+
+echo 'Job Finished!'
