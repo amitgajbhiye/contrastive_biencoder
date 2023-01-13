3852ef47eaddb4194dfc0d1dc57e46b6d9f682de
Amit Gajbhiye <amitgajbhiye>
2023-01-13 14:55:27 +0000

    con_split cs_wanli_cnetp5neg50thresh_prop_conj_je_mcrae_fintune

---
+++ run_cs_mcrae_5negcnetp_con_prop_pretrained_finetune copy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=csft_wanli_cnetp5neg50thresh_concept_split
+
+#SBATCH --output=logs/mcrae_finetune/out_cs_wanli_cnetp5neg50thresh_prop_conj_je_mcrae_fintune.txt
+#SBATCH --error=logs/mcrae_finetune/err_cs_wanli_cnetp5neg50thresh_prop_conj_je_mcrae_fintune.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100
+#SBATCH --mem=9G
+#SBATCH --gres=gpu:1
+
+#SBATCH --time 0-4:00:00
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 model/je_con_prop.py --config_file configs/3_finetune/cs_wanli_cnetp5neg50thresh_prop_conj_je_mcrae_fintune.json
+
+echo 'Job Finished !!!'
