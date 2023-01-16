f171c626cdb86786bd5e63818edd6d310dc40b68
Amit Gajbhiye <amitgajbhiye>
2023-01-16 11:23:20 +0000

    je_propconj_con_only_similar_prop_cnetp_pretrain

---
+++ run_je_propconj_con_only_similar_prop_cnetp_pretrain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=conOnlyPretrain
+
+#SBATCH --output=logs/data_sampling/out_run_je_propconj_con_only_similar_prop_cnetp_pretrain.txt
+#SBATCH --error=logs/data_sampling/err_run_je_propconj_con_only_similar_prop_cnetp_pretrain.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu
+#SBATCH --mem=8g
+#SBATCH --gres=gpu:1
+
+
+echo 'This script is running on:'
+hostname
+
+module load anaconda/2020.02
+
+. activate
+
+conda activate venv
+
+python3 model/je_property_conjuction.py --config_file configs/1_configs_pretrain/je_propconj_con_only_similar_prop_cnetp_pretrain.json
+
+echo 'Job Finished!'
