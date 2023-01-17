062880ea938cc8622a5a91d71d1da2639a96bb5e
Amit Gajbhiye <amitgajbhiye>
2023-01-17 12:57:55 +0000

    mcrae_ps_get_con_only_2_and_3sim_prop_conj_data

---
+++ run_mcrae_ps_get_con_only_2sim_prop_conj_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=McConOnly2Sim
+
+#SBATCH --output=logs/data_sampling/out_mcrae_ps_get_con_only_2sim_prop_conj_data.txt
+#SBATCH --error=logs/data_sampling/err_mcrae_ps_get_con_only_2sim_prop_conj_data.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+##SBATCH --partition gpu_v100,gpu
+##SBATCH --mem=8g
+##SBATCH --gres=gpu:1
+
+#SBATCH --partition compute
+#SBATCH --time 0-2:00:00
+
+echo 'This script is running on:'
+hostname
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/mcrae_ps_get_con_only_2sim_prop_conj_data.json
+
+echo 'Job Finished!'
