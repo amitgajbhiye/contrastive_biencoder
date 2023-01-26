0533c61c32310b495b2da412fd9914fd10eec464
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-01-26 12:22:18 +0000

    Wanli Pretrained direct mcrae finetuning

---
+++ run6_ps_direct_wanli_mcrae_finetune_predict_prop_3sim_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=WftPP
+
+#SBATCH --output=logs/mcrae_finetune/out_ps_mcrae_wanli_pre_hypo_pretrained_je_finetuned_on_mcrae_predict_property_3sim_data.txt
+#SBATCH --error=logs/mcrae_finetune/err_ps_mcrae_wanli_pre_hypo_pretrained_je_finetuned_on_mcrae_predict_property_3sim_data.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu
+#SBATCH --mem=9G
+#SBATCH --gres=gpu:1
+
+#SBATCH --time 0-5:00:00
+
+echo 'This script is running on:' hostname
+
+conda activate venv
+
+python3 model/je_property_conjuction.py --config_file configs/3_finetune/ps_mcrae_wanli_pre_hypo_pretrained_je_finetuned_on_mcrae_predict_property_3sim_data.json
+
+echo 'Job Finished !!!'
