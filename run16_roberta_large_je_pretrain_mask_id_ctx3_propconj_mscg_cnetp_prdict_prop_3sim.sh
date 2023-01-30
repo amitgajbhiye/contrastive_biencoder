bb257e4def606ca5ea9f5b74453472c63ecec8c8
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-01-30 18:41:58 +0000

    roberta_base_je_pretrain_mask_id_ctx3_propconj_mscg_cnetp_prdict_prop_3sim

---
+++ run16_roberta_large_je_pretrain_mask_id_ctx3_propconj_mscg_cnetp_prdict_prop_3sim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=rlMLMPPctx3Pretrain
+
+#SBATCH --output=logs/pretrain/out_roberta_large_je_pretrain_mask_id_ctx3_propconj_mscg_cnetp_prdict_prop_3sim.txt
+#SBATCH --error=logs/pretrain/err_roberta_large_je_pretrain_mask_id_ctx3_propconj_mscg_cnetp_prdict_prop_3sim.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu
+#SBATCH --mem=16g
+
+#SBATCH --time 2-00:00:00
+#SBATCH --gres=gpu:1
+
+echo 'This script is running on:' hostname
+
+conda activate venv
+
+python3 model/roberta_prop_aug.py --config_file configs/1_configs_pretrain/roberta_large_je_pretrain_mask_id_ctx3_propconj_mscg_cnetp_prdict_prop_3sim.json
+
+echo 'Job Finished!'
