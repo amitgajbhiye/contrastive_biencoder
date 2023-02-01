38f3c0be017921d4505363748713481581180453
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-01 16:45:39 +0000

    roberta_base_je_pretrain_mask_id_ctx1_con_prop_cnetp

---
+++ run20_roberta_base_je_pretrain_mask_id_ctx1_con_prop_cnetp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=cnetPrbConPropPretrain
+
+#SBATCH --output=logs/pretrain/out_roberta_base_je_pretrain_mask_id_ctx1_con_prop_cnetp.txt
+#SBATCH --error=logs/pretrain/err_roberta_base_je_pretrain_mask_id_ctx1_con_prop_cnetp.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu
+#SBATCH --mem=9g
+#SBATCH --gres=gpu:1
+
+##SBATCH --qos="gpu7d"
+
+#SBATCH -t 2-00:00:00
+
+echo 'This script is running on:' hostname
+
+conda activate venv
+
+python3 model/roberta_con_prop.py --config_file configs/1_configs_pretrain/roberta_base_je_pretrain_mask_id_ctx1_con_prop_cnetp.json
+
+echo 'Job Finished!'
