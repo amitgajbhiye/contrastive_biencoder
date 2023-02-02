eea8f0db701477f2623b3a5def09e95efde03cc0
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-02 15:45:06 +0000

    sim vocab

--- run24_get_embeds_concept_embeds_sim_vocab.sh
+++ run24_get_embeds_concept_embeds_sim_vocab.sh
@@ -9,18 +9,17 @@
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-##SBATCH -p gpu_v100,gpu
-##SBATCH --mem=8g
-##SBATCH --gres=gpu:1
-
 #SBATCH -p gpu
-#SBATCH -t 0-02:00:00
+#SBATCH --mem=8g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-01:00:00
 
 echo 'This script is running on:' hostname
 
 conda activate venv
 
-python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/word_similarity_get_concepts_embeddings.json
 
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/word_similarity_get_concepts_embeddings.json
 
 echo 'Job finished!'
