5118b38c698e1f0598852e729b2e2a4fa84f5c01
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-07 09:57:34 +0000

    con prop ps finetune

---
+++ run5_ps_mcrae_con_prop_bert_base_mask_id_ctx1_cnetp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=bbPSMcFT
+
+#SBATCH --output=logs/mcrae_finetune/out_ps_mcrae_con_prop_bert_base_mask_id_ctx1_cnetp.txt
+#SBATCH --error=logs/mcrae_finetune/err_ps_mcrae_con_prop_bert_base_mask_id_ctx1_cnetp.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100
+#SBATCH --mem=8g
+#SBATCH --gres=gpu:1
+
+##SBATCH --qos="gpu7d"
+
+#SBATCH -t 0-05:00:00
+
+conda activate venv
+
+python3 model/lm_con_prop.py --config_file configs/3_finetune/ps_mcrae_con_prop_bert_base_mask_id_ctx1_cnetp.json
+
+echo 'Job Finished!'
