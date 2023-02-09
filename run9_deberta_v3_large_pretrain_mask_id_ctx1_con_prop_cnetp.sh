2e906659ab726d0360d09cbd3307796a0f563215
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-09 09:42:19 +0000

    deberta_v3_large_pretrain_mask_id_ctx1_con_prop_cnetp

--- run9_deberta_v3_large_pretrain_mask_id_ctx1_con_prop_cnetp.sh
+++ run9_deberta_v3_large_pretrain_mask_id_ctx1_con_prop_cnetp.sh
@@ -9,8 +9,8 @@
 #SBATCH --ntasks=5
 #SBATCH --account scw1858
 
-#SBATCH --partition gpu_v100
-#SBATCH --mem=16g
+#SBATCH --partition gpu
+#SBATCH --mem=0
 #SBATCH --gres=gpu:1
 
 ##SBATCH --qos="gpu7d"
