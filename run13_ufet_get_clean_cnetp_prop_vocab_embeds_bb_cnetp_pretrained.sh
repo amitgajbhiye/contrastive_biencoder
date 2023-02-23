9a243b4ac671a7e5fcb73fbc3938b074b6ae5ec2
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-23 15:22:43 +0000

    ufet_get_clean_cnetp_prop_vocab_embeds_bb_cnetp_pretrained

---
+++ run13_ufet_get_clean_cnetp_prop_vocab_embeds_bb_cnetp_pretrained.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=ufetPropVocab
+
+#SBATCH --output=logs/ufet_exp/out_ufet_get_clean_cnetp_prop_vocab_embeds_bb_cnetp_pretrained.txt
+#SBATCH --error=logs/ufet_exp/err_ufet_ufet_get_clean_cnetp_prop_vocab_embeds_bb_cnetp_pretrained.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+##SBATCH -p gpu_v100,gpu
+##SBATCH --gres=gpu:1
+
+#SBATCH -p htc
+#SBATCH --mem=8G
+#SBATCH -t 0-02:00:00
+
+echo $SLURM_JOB_ID
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/ufet/ufet_get_clean_cnetp_prop_vocab_embeds_bb_cnetp_pretrained.json
+
+echo 'Job Finished!'
