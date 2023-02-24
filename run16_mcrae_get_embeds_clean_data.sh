1fad3d26c0c160f5225d67858bccc57da132411a
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-24 11:23:05 +0000

    ps_mcrae_hp_prop_conj_ctx5_roberta_large_je_fintune_thresh565

---
+++ run16_mcrae_get_embeds_clean_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=getEmbeds
+
+#SBATCH --output=logs/mcrae_clean_vocab_data/out_mcrae_get_embeds_clean_data.txt
+#SBATCH --error=logs/mcrae_clean_vocab_data/err_mcrae_get_embeds_clean_data.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=8G
+#SBATCH -t 0-02:00:00
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/mcrae_get_con_embeddings.json
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/mcrae_get_prop_embeddings.json
+python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/get_clean_prop_vocab_embeddings.json
+
+echo 'Job Finished!'
