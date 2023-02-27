bfcbdddf8cf06dad92afaba55bec4b28279333c2
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-27 13:45:37 +0000

    ufet_zero_shot_deberta_v3_large_je_5neg_filter_sim_props_thres50

--- run19_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.sh
+++ run19_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.sh
@@ -9,11 +9,11 @@
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p gpu_v100
+#SBATCH -p gpu,gpu_v100
 #SBATCH --gres=gpu:1
 
 #SBATCH --mem=0
-#SBATCH -t 0-10:00:00
+#SBATCH -t 0-09:00:00
 
 conda activate venv
 
