a2984876c9f95961f2e1e608180bc6014bb4dc82
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-02-27 12:15:00 +0000

    ufet_zero_shot_deberta_v3_large_je_5neg_filter_sim_props_thres50

--- run19_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.sh
+++ run19_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.sh
@@ -2,22 +2,21 @@
 
 #SBATCH --job-name=filterUfetCon50Sim 
 
-#SBATCH --output=logs/ufet_exp/out_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.txt
-#SBATCH --error=logs/ufet_exp/err_ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.txt
+#SBATCH --output=logs/ufet_exp/out_ufet_zero_shot_deberta_v3_large_je_5neg_filter_sim_props_thres50.txt
+#SBATCH --error=logs/ufet_exp/err_ufet_zero_shot_deberta_v3_large_je_5neg_filter_sim_props_thres50.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-##SBATCH -p gpu
-##SBATCH --gres=gpu:1
+#SBATCH -p gpu_v100
+#SBATCH --gres=gpu:1
 
-#SBATCH -p compute
-#SBATCH --mem=42G
-#SBATCH -t 2-00:00:00
+#SBATCH --mem=0
+#SBATCH -t 0-10:00:00
 
 conda activate venv
 
-python3 je_filter_similar_props.py --config_file configs/2_configs_get_embeds_and_train_data/ufet/ufet_deberta_v3_large_je_5neg_filter_sim_props_thres40.json
+python3 je_filter_similar_props.py --config_file configs/2_configs_get_embeds_and_train_data/ufet/ufet_zero_shot_deberta_v3_large_je_5neg_filter_sim_props_thres50.json
 
 echo 'Job Finished!'
