e01c04dbe7a25c16e031e311a9ad3f9b64c77c2d
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-03-07 13:03:30 +0000

    bienc_bert_large_cnetp_pretrain_lr26

---
+++ run2_bienc_bert_large_cnetp_pretrain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=BLpCP26
+
+#SBATCH --output=logs/pretrain/out_bienc_bert_large_cnetp_pretrain_lr26.txt
+#SBATCH --error=logs/pretrain/err_bienc_bert_large_cnetp_pretrain_lr26.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100
+#SBATCH --mem=16g
+#SBATCH --gres=gpu:1
+
+#SBATCH --time 2-00:00:00
+
+##SBATCH --qos=gu7d
+
+
+conda activate venv
+
+python3 bienc_run_model.py --config_file configs/1_configs_pretrain/bienc_bert_large_cnetp_pretrain_lr26.json
+
+echo 'Job Finished!'
