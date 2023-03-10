3b1e0b97d904f4a51cae5f42be7cfa5f7fa4a470
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-03-10 10:32:05 +0000

    cslb_pretrain_deberta_v3_large_mask_id_ctx1_cslb_1neg_con_prop

---
+++ run5_cslb_pretrain_deberta_v3_large_mask_id_ctx1_cslb_2neg_con_prop.json.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=2NconPropCSLBPretrain
+
+#SBATCH --output=logs/pretrain/out_cslb_pretrain_deberta_v3_large_mask_id_ctx1_cslb_2neg_con_prop.txt
+#SBATCH --error=logs/pretrain/err_cslb_pretrain_deberta_v3_large_mask_id_ctx1_cslb_2neg_con_prop.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100
+#SBATCH --mem=32g
+#SBATCH --gres=gpu:1
+
+#SBATCH --time 2-00:00:00
+
+conda activate venv
+
+python3 model/lm_con_prop.py --config_file configs/1_configs_pretrain/cslb_pretrain_deberta_v3_large_mask_id_ctx1_cslb_2neg_con_prop.json
+
+echo 'Job Finished!'
