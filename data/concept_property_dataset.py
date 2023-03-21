6ae068df89e6a260ab712cb42f03665396693d75
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-03-21 14:47:36 +0000

    bienc_contra_bert_base_cnetp_pretrain

--- data/concept_property_dataset.py
+++ data/concept_property_dataset.py
@@ -27,7 +27,7 @@ class ConceptPropertyDataset(Dataset):
                 sep="\t",
                 header=None,
                 names=["concept", "property"],
-            )
+            )[0:2000]
 
             self.data_df.drop_duplicates(inplace=True)
             self.data_df.dropna(inplace=True)
