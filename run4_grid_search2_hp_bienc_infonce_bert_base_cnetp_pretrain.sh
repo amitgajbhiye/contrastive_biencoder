f9c83396e724707e97c9e95ee99d03ee696a8582
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-03-30 17:49:49 +0100

    grid_search2_hp_bienc_infonce_bert_base_cnetp_pretrain

--- run4_grid_search2_hp_bienc_infonce_bert_base_cnetp_pretrain.sh
+++ run4_grid_search2_hp_bienc_infonce_bert_base_cnetp_pretrain.sh
@@ -9,16 +9,14 @@
 #SBATCH --ntasks=5
 #SBATCH --account scw1858
 
-#SBATCH --partition highmem
+#SBATCH --partition gpu_v100
 
-#SBATCH --time 0-01:00:00
+#SBATCH --time 7-00:00:00
 
-##SBATCH --gres=gpu:1
-##SBATCH --qos="gpu7d"
+#SBATCH --gres=gpu:1
+#SBATCH --qos="gpu7d"
 
 #SBATCH --mem=10G
-
-
 
 conda activate venv
 
