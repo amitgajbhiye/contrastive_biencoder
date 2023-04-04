176e6ead029ca72a3e21d15d3a54a6cb9e9e029e
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-04-04 18:53:27 +0100

    grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp

--- run8_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.sh
+++ run8_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.sh
@@ -9,11 +9,11 @@
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p gpu_v100,gpu
+#SBATCH -p gpu_v100
 #SBATCH --mem=10g
 #SBATCH --gres=gpu:1
-
-#SBATCH -t 2-00:00:00
+#SBATCH --qos="gpu7d"
+#SBATCH -t 7-00:00:00
 
 
 conda activate venv
