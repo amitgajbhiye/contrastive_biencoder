1ecaf3c0beba32f718f54fc7f9448fe1d0e4a8ef
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-04-05 10:57:14 +0100

    grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp

--- run8_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.sh
+++ run8_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
 
-#SBATCH --job-name=1gsBestPretrainedModel
+#SBATCH --job-name=ep4GS_BPM-BestPretrainedModelSearch
 
-#SBATCH --output=logs/mcrae_fine_tune/out_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.txt
-#SBATCH --error=logs/mcrae_fine_tune/err_grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.txt
+#SBATCH --output=logs/mcrae_fine_tune/out_grid1_ep4_search_best_model_mcrae_pcv_bb_cbien_cnetp.txt
+#SBATCH --error=logs/mcrae_fine_tune/err_grid1_ep4_search_best_model_mcrae_pcv_bb_cbien_cnetp.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
@@ -12,12 +12,13 @@
 #SBATCH -p gpu_v100
 #SBATCH --mem=10g
 #SBATCH --gres=gpu:1
+
 #SBATCH --qos="gpu7d"
 #SBATCH -t 7-00:00:00
 
 
 conda activate venv
 
-python3 bienc_fine_tune.py --config_file configs/3_finetune/grid1_search_best_model_mcrae_pcv_bb_cbien_cnetp.json
+python3 bienc_fine_tune.py --config_file configs/3_finetune/grid1_ep4_search_best_model_mcrae_pcv_bb_cbien_cnetp.json
 
 echo 'Job Finished !!!'
