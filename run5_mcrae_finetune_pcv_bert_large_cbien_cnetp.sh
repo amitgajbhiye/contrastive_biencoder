f8ce0956cd4994ca961050b073a0408818bfa1d8
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-04-12 14:34:10 +0100

    mcrae_finetune_pcv_bert_large_cbien_cnetp

---
+++ run5_mcrae_finetune_pcv_bert_large_cbien_cnetp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=MRblFT
+
+#SBATCH --output=logs/mcrae_fine_tune/out_mcrae_finetune__pcv_bert_large_cbien_cnetp.txt
+#SBATCH --error=logs/mcrae_fine_tune/err_mcrae_finetune__pcv_bert_large_cbien_cnetp.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --mem=10g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-08:00:00
+
+
+conda activate venv
+
+python3 bienc_fine_tune.py --config_file configs/3_finetune/mcrae_finetune_pcv_bert_large_cbien_cnetp.json
+
+echo 'Job Finished !!!'
