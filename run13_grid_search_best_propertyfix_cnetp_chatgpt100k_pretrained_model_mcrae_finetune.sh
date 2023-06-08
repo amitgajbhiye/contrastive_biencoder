0b3b761a13ad7fc7409b3a3b177c98114632d27b
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-08 12:22:52 +0100

    propertyfix pretrained best model search bert large

--- run13_grid_search_best_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.sh
+++ run13_grid_search_best_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.sh
@@ -2,19 +2,19 @@
 
 #SBATCH --job-name=GSbmPropFix
 
-#SBATCH --output=logs/chatgpt_mcrae_fine_tune/out_grid_search_best_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.txt
-#SBATCH --error=logs/chatgpt_mcrae_fine_tune/err_grid_search_best_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.txt
+#SBATCH --output=logs/chatgpt_mcrae_fine_tune/out_grid_search_best_all_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.txt
+#SBATCH --error=logs/chatgpt_mcrae_fine_tune/err_grid_search_best_all_propertyfix_cnetp_chatgpt100k_pretrained_model_mcrae_finetune.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p gpu_v100,gpu
+#SBATCH -p gpu_v100
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
+#SBATCH --qos="gpu7d"
 
-#SBATCH -t 2-00:00:00
-
+#SBATCH -t 7-00:00:00
 
 conda activate venv
 
